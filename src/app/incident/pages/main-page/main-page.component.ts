fc6bbc80aada28219a1d441809a6079ec0bff13c
= <=>
2024-02-23 08:13:38 -0500

    Add MainPage

--- src/app/incident/pages/main-page/main-page.component.ts
+++ src/app/incident/pages/main-page/main-page.component.ts
@@ -1,14 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MaterialModule } from 'src/app/material/material.module';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
   imports: [
     CommonModule,
+    MaterialModule
   ],
   templateUrl: `./main-page.component.html`,
   styleUrls: ['./main-page.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MainPageComponent { }
+export class MainPageComponent {
+
+  pathImage:string = 'assets/images/logo.png';
+
+ }
